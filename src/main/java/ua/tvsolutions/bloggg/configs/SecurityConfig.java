adbaf0837f2c418417e794c29b7128d48ebbb8f4
darrrkus <darrrkus@gmail.com>
2022-05-09 20:34:12 +0300

    first commit, works only user login

--- src/main/java/ua/tvsolutions/bloggg/configs/SecurityConfig.java
+++ src/main/java/ua/tvsolutions/bloggg/configs/SecurityConfig.java
@@ -23,7 +23,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/admin/**").hasAuthority("ADMIN")
+                .antMatchers("/admin/**").hasRole("ADMIN")
                 .antMatchers("/profile/**").authenticated()
                 .and()
                 .formLogin()
